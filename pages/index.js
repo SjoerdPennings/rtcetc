385adb7b3aac823f3346e131bb61a23229227613
Sjoerd Pennings <sjoerd@dispi.com>
2022-07-08 15:42:36 +0200

    Rename + Enter key

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,33 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter();
+
+  function searchCard() {
+    router.push('/card/' + encodeURIComponent(document.getElementById('card').value));
+  }
+
+  function handleEnter(event)  {
+    if (event.code == 'Enter') {
+      searchCard()
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keypress', handleEnter);
+    return () => {
+      window.removeEventListener('keypress', handleEnter)
+    }
+  })
+
   return (
     <>
-      <h1>RTCETC</h1>
-      <input type="text" name="card" id="card" />
-      <button onClick={() => router.push('/card/' + encodeURIComponent(document.getElementById('card').value))}>Submit</button>
+      <h1>👏 Reading 👏 The 👏 Card 👏 Explains 👏 The 👏 Card 👏</h1>
+      <input type="text" placeholder="Pick a card." name="card" id="card" />
+      <button onClick={searchCard}>Submit</button>
     </>
   )
 }
