0697c15bd1aaa06025156c247073ccee6b94991f
SjoerdPennings <2182351+SjoerdPennings@users.noreply.github.com>
2022-07-11 12:05:44 +0200

    error checking?

--- pages/card/[card].js
+++ pages/card/[card].js
@@ -2,29 +2,47 @@ import Link from 'next/link';
 import { getCard } from '../../lib/scry.js';
 
 function Card({objCard}) {
-    return (
-        <>
-        <h1>{objCard.name} - {objCard.cost}</h1>
-        <h2>{objCard.type}</h2>
-        <p>⚔️ {objCard.pow} / 🛡️ {objCard.def}</p>
-        <p className='rules'>{objCard.rules}</p><br/>
-        <Link href='/'>
-            <a>Go back!</a>
-        </Link>
-        </>
-    );
+    if (objCard.error == false) {
+        return (
+            <>
+            <h1>{objCard.name} - {objCard.cost}</h1>
+            <h2>{objCard.type}</h2>
+            <p>⚔️ {objCard.pow} / 🛡️ {objCard.def}</p>
+            <p className='rules'>{objCard.rules}</p><br/>
+            <Link href='/'>
+                <a>Go back!</a>
+            </Link>
+            </>
+        );
+    } else {
+        return (
+            <>
+            <h1>Error!</h1>
+            <p className='rules'>There's no such card!</p><br/>
+            <Link href='/'>
+                <a>Go back!</a>
+            </Link>
+            </>
+        );
+    }
 }
 
 export async function getServerSideProps({params}) {
     const data = await getCard(params.card)
-
-    const objCard = {
-        name: data.data[0].name,
-        cost: data.data[0].mana_cost,
-        type: data.data[0].type_line,
-        pow: data.data[0].power ?? '―',
-        def: data.data[0].toughness ?? '―',
-        rules: data.data[0].oracle_text
+    if (data.object != 'error') {
+        const objCard = {
+            name: data.data[0].name,
+            cost: data.data[0].mana_cost,
+            type: data.data[0].type_line,
+            pow: data.data[0].power ?? '―',
+            def: data.data[0].toughness ?? '―',
+            rules: data.data[0].oracle_text,
+            error: false,
+        }
+    } else {
+        const objCard = {
+            error: true
+        }
     }
 
     return {
